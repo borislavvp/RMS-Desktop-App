f9a027b034a51adb7cf84cb3834ae18eb96dad71
Borislav Pavlov <416657@student.fontys.nl>
2021-03-07 16:59:21 +0200

    added tailwind

---
+++ desktop-app/postcss.config.js
@@ -0,0 +1,9 @@
+const plugins = [
+  require("postcss-import"),
+  require("tailwindcss"),
+  require("autoprefixer")
+];
+
+module.exports = {
+  plugins: plugins
+};
---
+++ desktop-app/tailwind.config.js
@@ -0,0 +1,34 @@
+const { fontFamily, spacing } = require("tailwindcss/defaultTheme");
+module.exports = {
+   purge: {
+    // Uncomment for faster build time
+    // enabled:true,
+    enabled:
+      process.env.NODE_ENV === "production",
+    content: [
+      "./src/**/*.pcss",
+      "./src/**/*.css",
+      "./src/**/*.sass",
+      "./src/**/*.html",
+      "./src/**/*.js",
+      "./src/**/*.ts"
+    ],
+  },
+  darkMode: 'class', // or 'media' or false
+  theme: {
+    extend: {
+      maxHeight: {
+        ...spacing
+      },
+      fontFamily: {
+        sans: ["Open Sans", "Source Sans Pro", ...fontFamily.sans]
+      },
+    },
+  },
+  variants: {
+    extend: {
+      boxShadow: ["responsive", "hover", "focus", "group-hover"]
+    },
+  },
+  plugins: [],
+}
