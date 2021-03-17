610b1aee66a5169f6280088a538a4d71062043c6
Borislav Pavlov <416657@student.fontys.nl>
2021-03-17 23:03:04 +0200

    added the initial routes and the side bar component

--- desktop-app/src/app/app.module.ts
+++ desktop-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './shared/side-bar/side-bar.component';
+import { OrdersTableComponent } from './orders/orders-table/orders-table.component';
+import { MealsTableComponent } from './meals/meals-table/meals-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    OrdersTableComponent,
+    MealsTableComponent
   ],
   imports: [
     BrowserModule,
