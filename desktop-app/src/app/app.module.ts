85b85582171bfc1cdeac345d9731572be1308dd8
Borislav Pavlov <416657@student.fontys.nl>
2021-03-20 10:27:03 +0200

    changed the order table to include the order options and changed some stylings

--- desktop-app/src/app/app.module.ts
+++ desktop-app/src/app/app.module.ts
@@ -6,10 +6,13 @@ import { AppComponent } from './app.component';
 import { SideBarComponent } from './components/shared/side-bar/side-bar.component';
 import { OrdersTableComponent } from './components/orders/orders-table/orders-table.component';
 import { MealsTableComponent } from './components/meals/meals-table/meals-table.component';
-import { BaseDropdownComponent } from './components/shared/dropdown/base-dropdown/base-dropdown.component';
+import { BaseDropdownComponent } from './components/shared/dropdown/base-dropdown.component';
 import { OrderStatusComponent } from './components/orders/order-status/order-status.component';
 import { OrderHeaderComponent } from './components/orders/order-header/order-header.component';
 import { OrdersPageComponent } from './pages/orders/orders-page/orders-page.component';
+import { DropdownMenuDirective } from './directives/dropdown-menu-directive.directive';
+import { DropdownMenuOriginDirective } from './directives/dropdown-menu-origin.directive';
+import { OrderOptionsComponent } from './components/orders/order-options/order-options.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +24,9 @@ import { OrdersPageComponent } from './pages/orders/orders-page/orders-page.comp
     OrderStatusComponent,
     OrderHeaderComponent,
     OrdersPageComponent,
+    DropdownMenuDirective,
+    DropdownMenuOriginDirective,
+    OrderOptionsComponent,
   ],
   imports: [
     BrowserModule,
--- desktop-app/src/app/components/orders/orders-table/orders-table.component.ts
+++ desktop-app/src/app/components/orders/orders-table/orders-table.component.ts
@@ -8,16 +8,19 @@ import { Order, OrderStatus } from '../models/order.model';
 export class OrdersTableComponent implements OnInit {
   headers = ['Order ID', 'Date', 'Customer Name', 'Location', 'Amount', 'Status'];
   orders = [
-    new Order(1,new Date().toDateString(),"boby","Bomanshof 131",19,OrderStatus.New),
-    new Order(1,new Date().toDateString(),"boby","Bomanshof 131",19,OrderStatus.Preparing),
-    new Order(1,new Date().toDateString(),"boby","Bomanshof 131",19,OrderStatus.Delivered),
-    new Order(1,new Date().toDateString(),"boby","Bomanshof 131",19,OrderStatus.Delivering),
-    new Order(1,new Date().toDateString(),"boby","Bomanshof 131",19,OrderStatus.New),
-    new Order(1,new Date().toDateString(),"boby","Bomanshof 131",19,OrderStatus.New),
-    new Order(1,new Date().toDateString(),"boby","Bomanshof 131",19,OrderStatus.New),
+    new Order(1,new Date().toDateString(),"boby","Bomanshof 131",19,OrderStatus.New,[]),
+    new Order(1,new Date().toDateString(),"boby","Bomanshof 131",19,OrderStatus.Preparing,[]),
+    new Order(1,new Date().toDateString(),"boby","Bomanshof 131",19,OrderStatus.Delivered,[]),
+    new Order(1,new Date().toDateString(),"boby","Bomanshof 131",19,OrderStatus.Delivering,[]),
+    new Order(1,new Date().toDateString(),"boby","Bomanshof 131",19,OrderStatus.New,[]),
+    new Order(1,new Date().toDateString(),"boby","Bomanshof 131",19,OrderStatus.New,[]),
+    new Order(1,new Date().toDateString(),"boby","Bomanshof 131",19,OrderStatus.New,[]),
   ];
-  columns = Object.keys(this.orders[0]);
+  private columns = Object.keys(this.orders[0]);
  
+  public getColumns() {
+    return this.columns.filter(column => column !== 'meals');
+  }
   constructor() { }
 
   ngOnInit(): void {
