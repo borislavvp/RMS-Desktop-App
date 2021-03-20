7cd62fbb0f1c4e65243421c92def514561d7b65d
Borislav Pavlov <416657@student.fontys.nl>
2021-03-20 10:26:23 +0200

    added order options component

---
+++ desktop-app/src/app/components/orders/order-options/order-options.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Order, OrderStatus } from '../models/order.model';
+
+@Component({
+  selector: 'app-order-options',
+  templateUrl: './order-options.component.html',
+})
+export class OrderOptionsComponent implements OnInit {
+  @Input() order: Order;
+  public get OrderStatus(): typeof OrderStatus{
+    return OrderStatus;
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
