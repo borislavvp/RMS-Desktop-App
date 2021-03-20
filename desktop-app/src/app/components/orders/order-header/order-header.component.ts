2f187754217e10c04fe340b4141231a45bc92c27
Borislav Pavlov <416657@student.fontys.nl>
2021-03-20 10:45:07 +0200

    updated the styling of the header component

--- desktop-app/src/app/components/orders/order-header/order-header.component.ts
+++ desktop-app/src/app/components/orders/order-header/order-header.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-order-header',
   templateUrl: './order-header.component.html',
-  styleUrls: ['./order-header.component.scss']
 })
 export class OrderHeaderComponent implements OnInit {
 
