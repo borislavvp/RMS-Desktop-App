7271d258375157370298ba884d602293d0012969
Borislav Pavlov <416657@student.fontys.nl>
2021-03-21 09:38:29 +0200

    added initial version of the order details page

--- desktop-app/src/app/app-routing.module.ts
+++ desktop-app/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MealsTableComponent } from './components/meals/meals-table/meals-table.component';
+import { OrderDetailsPageComponent } from './pages/orders/order-details-page/order-details-page.component';
 import { OrdersPageComponent } from './pages/orders/orders-page/orders-page.component';
 
 const routes: Routes = [
   {path: 'orders', component: OrdersPageComponent},
+  {path: 'orders/:id', component: OrderDetailsPageComponent},
   {path: 'meals', component: MealsTableComponent},
 ];
 
--- desktop-app/src/app/pages/orders/order-details-page/order-details-page.component.ts
+++ desktop-app/src/app/pages/orders/order-details-page/order-details-page.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-order-details-page',
   templateUrl: './order-details-page.component.html',
 })
 export class OrderDetailsPageComponent implements OnInit {
-
-  constructor() { }
+  orderId: number;
+  constructor(
+    private route: ActivatedRoute,) { }
 
   ngOnInit(): void {
+    this.route.params
+      .subscribe(
+        async (params: Params) => {
+          
+            this.orderId = +params['id'];
+          }
+      );
   }
 
 }
