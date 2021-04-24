c8a4309415843cad485f6e00bc3f43b9ede41cfe
Borislav Pavlov <416657@student.fontys.nl>
2021-04-24 11:40:59 +0200

    added auth guard

--- desktop-app/src/app/services/authentication/auth-guard.service.ts
+++ desktop-app/src/app/services/authentication/auth-guard.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
+import { from } from 'rxjs';
+import { take,tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardService implements CanActivate {
   constructor(public auth: AuthService, public router: Router) {}
-  canActivate(): boolean {
-    if (!this.auth.isAuthenticated()) {
-      this.router.navigate(['login']);
-      return false;
-    }
-    return true;
+  async canActivate( route: ActivatedRouteSnapshot,
+      state: RouterStateSnapshot) : Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      this.auth.isAuthenticated.then(logged => {
+        if (!logged) {
+          return reject(this.router.navigate(['login']));
+        }
+        return resolve(true);
+      })
+    })
   }
 }
