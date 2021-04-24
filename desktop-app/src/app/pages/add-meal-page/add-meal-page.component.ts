ec4384955ba122a49cd545cb6863d8f5e5e6bce7
Borislav Pavlov <416657@student.fontys.nl>
2021-04-24 11:39:24 +0200

    deleted the scss file

--- desktop-app/src/app/pages/add-meal-page/add-meal-page.component.ts
+++ desktop-app/src/app/pages/add-meal-page/add-meal-page.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-add-meal-page',
   templateUrl: './add-meal-page.component.html',
-  styleUrls: ['./add-meal-page.component.scss']
 })
 export class AddMealPageComponent implements OnInit {
 
