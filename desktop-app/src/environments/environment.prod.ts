8d9729b3d3031349e947994323ec7eddbd836703
Borislav Pavlov <416657@student.fontys.nl>
2021-05-09 18:41:39 +0300

    added the variable to the prod env

--- desktop-app/src/environments/environment.prod.ts
+++ desktop-app/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
-  production: true,
+  PRODUCTION: true,
   //TODO: To be changed when deployed
-  IDENTITY_AUTHORITY: "https://localhost:5001"
+  IDENTITY_AUTHORITY: "https://localhost:5001",
+  ORDER_MESSAGE_SERVICE:(token:string) => `ws://localhost:3333/authorization?token=${token}`,
 };
