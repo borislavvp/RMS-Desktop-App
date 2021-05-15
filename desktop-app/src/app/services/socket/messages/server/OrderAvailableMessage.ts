31d3e0f0600ba1c277d38a2c19f354597d80b30b
Borislav Pavlov <416657@student.fontys.nl>
2021-05-15 15:52:45 +0300

    removed log statement and changed a little the scoket messages

--- desktop-app/src/app/services/socket/messages/server/OrderAvailableMessage.ts
+++ desktop-app/src/app/services/socket/messages/server/OrderAvailableMessage.ts
@@ -1,12 +1,15 @@
+import { Meal } from "src/app/models/meal.model";
 import { Message } from "../Message";
 import { ServerMessageType } from "./ServerMessageType";
 
 interface OrderAvailablePayload {
-    orderNumber: number;
-    orderDate: string,
-    customerName: string,
-    location: string,
-    amount:number
+  orderNumber: number;
+  orderDate: string,
+  customerName: string,
+  customerPhone: string,
+  location: string,
+  amount: number,
+  products:Meal[]
 }
 
 export class OrderAvailableMessage implements Message {
