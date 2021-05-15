829094ae7f31cf8f9c5be1d6c1d9e9b59a8ad2ea
Borislav Pavlov <416657@student.fontys.nl>
2021-05-15 16:13:50 +0300

    added env variable to the orders microservice

--- desktop-app/src/environments/environment.prod.ts
+++ desktop-app/src/environments/environment.prod.ts
@@ -3,4 +3,5 @@ export const environment = {
   //TODO: To be changed when deployed
   IDENTITY_AUTHORITY: "https://localhost:5001",
   ORDER_MESSAGE_SERVICE:"ws://localhost:3333",
+  ORDERS_MICROSERVICE: "https://localhost:5050/api/orders"
 };
--- desktop-app/src/environments/environment.ts
+++ desktop-app/src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   PRODUCTION: false,
   IDENTITY_AUTHORITY: "https://localhost:5001",
-  ORDER_MESSAGE_SERVICE:"ws://localhost:3333",
+  ORDER_MESSAGE_SERVICE: "ws://localhost:3333",
+  ORDERS_MICROSERVICE: "https://localhost:5050/api/orders"
 };
 
 /*
