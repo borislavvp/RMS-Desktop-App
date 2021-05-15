64fa4fb3e6c8fb68567746cd9679476a5490b119
Borislav Pavlov <416657@student.fontys.nl>
2021-05-15 16:13:31 +0300

    changed the pagination info to be dynamic

--- desktop-app/src/app/components/orders/orders-table/orders-table.component.ts
+++ desktop-app/src/app/components/orders/orders-table/orders-table.component.ts
@@ -1,5 +1,6 @@
 import { Component, HostListener, Input, OnInit } from '@angular/core';
-import { Dummy_Orders} from '../../../models/order.model';
+import { OrdersService } from 'src/app/services/orders/orders.service';
+import { Order} from '../../../models/order.model';
 @Component({
   selector: 'app-orders-table',
   templateUrl: './orders-table.component.html',
@@ -7,29 +8,21 @@ import { Dummy_Orders} from '../../../models/order.model';
 })
 export class OrdersTableComponent implements OnInit {
   @Input() page: number;
-  orders = Dummy_Orders;
-  headers = ['Order ID', 'Date', 'Customer Name', 'Location', 'Amount', 'Status'];
+  @Input() itemsPerPage: number;
+  orders: Order[] = [];
+  headers = ['Order ID', 'Date', 'Customer Name', 'Location', 'Phone', 'Amount', 'Status'];
   
-  private columns = Object.keys(this.orders[0]);
+  constructor(ordersService: OrdersService) {
+    ordersService.ordersChanged.subscribe(orders => {
+      this.orders = orders;
+    })
+    this.orders = ordersService.Orders;
+   }
   
-  public getColumns = () => this.columns.filter(column => column !== 'meals');
+  public getColumns = () => this.orders.length > 0 ? Object.keys(this.orders[0]).filter(column => column !== 'products') : [];
   
-  public getOrdersNumber = () => this.orders.length;
-
-  public IsScreenBigger = () => innerHeight > 800 && innerWidth > 1450;
-
-  constructor() { }
-  
-  public innerHeight: any;
-  public innerWidth: any;
-
   ngOnInit(): void {
-  }
 
-  @HostListener('window:resize', ['$event'])
-  onResize(event) {
-    this.innerHeight = window.innerHeight;
-    this.innerWidth = window.innerWidth;
   }
 
 }
--- desktop-app/src/app/pages/orders/orders-page/orders-page.component.ts
+++ desktop-app/src/app/pages/orders/orders-page/orders-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { OrdersService } from 'src/app/services/orders/orders.service';
 
 @Component({
   selector: 'app-orders-page',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersPageComponent implements OnInit {
   page: number = 1;
-  constructor() { }
+  numberOfOrders: number = 0;
+  numberOfOrdersVisible: number = 0;
+  innerHeight: any;
+  innerWidth: any;
 
+  constructor(ordersService: OrdersService) {
+    ordersService.ordersChanged.subscribe(orders => {
+      this.numberOfOrders = orders.length;
+      this.numberOfOrdersVisible = this.OrdersPerPage * this.page > orders.length ? orders.length : this.OrdersPerPage * this.page;
+    })
+}
+  
+  public IsScreenBigger = () => innerHeight > 800 && innerWidth > 1450;
+  
+  get OrdersPerPage() {
+    return this.IsScreenBigger() ? 10 : 7;
+  }
   ngOnInit(): void {
+  }
+  
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerHeight = window.innerHeight;
+    this.innerWidth = window.innerWidth;
   }
 
 }
