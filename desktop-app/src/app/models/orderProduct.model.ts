34d3d7e7777375965617dafbb203cd7d9df6c1f9
Borislav Pavlov <416657@student.fontys.nl>
2021-05-25 11:56:25 +0300

    added order product model

---
+++ desktop-app/src/app/models/orderProduct.model.ts
@@ -0,0 +1,3 @@
+export class OrderProduct{
+    constructor(public id: number, public name: string,public ingredients: string,public price:number,public quantity:number,public imageUrl: string){}
+}
